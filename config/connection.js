4b10720e6c45b77334d0fe6eb0b020651e2cc54c
dennismacc <dennis.maccarthy@gmail.com>
2022-07-20 13:29:54 -0700

    work on connection, add .env, install more packages

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,14 @@
 const mysql = require("mysql2");
 const util = require("util");
 
+require("dotenv").config();
+
 const db = mysql.createConnection({
-  user: "root",
-  password: "woot",
-  host: "localhost",
-  database: "employees",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 8889,
 });
 
 db.query = util.promisify(db.query);
